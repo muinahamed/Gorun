67e3e64269bd795e65ed42118a812abbdd016f93
muin1234 <muin.ahamed@kadritech.se>
2023-08-30 11:02:14 +0600

    update

--- src/module/signup/MobileSignup.js
+++ src/module/signup/MobileSignup.js
@@ -1,11 +1,14 @@
 /* eslint-disable react-native/no-inline-styles */
 import {useNavigation} from '@react-navigation/native';
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
+  Button,
   KeyboardAvoidingView,
   Platform,
+  SafeAreaView,
   ScrollView,
   StyleSheet,
+  TextInput,
   View,
 } from 'react-native';
 import MText, {
@@ -30,6 +33,7 @@ import ScreenWrapper from '../../common/ScreenWrapper';
 import {PHONE_CIRCLE} from '../../image/SvgPath';
 import {windowWidth} from '../../utils/Measure';
 import Header from '../../common/Header';
+import auth from '@react-native-firebase/auth';
 
 const MobileSignup = () => {
   const navigation = useNavigation();
@@ -37,11 +41,55 @@ const MobileSignup = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [formattedPhoneNumber, setFormattedPhoneNumber] = useState('');
   const [data, setData] = useState({countryShortName: 'NG'});
+  const [confirm, setConfirm] = useState(null);
+  // verification code (OTP - One-Time-Passcode)
+  const [code, setCode] = useState('');
   const phoneInput = useRef(null);
 
   const onSendOtpPressed = async () => {
     navigation.navigate('otpVerification');
   };
+
+  // Handle login
+  function onAuthStateChanged(user) {
+    if (user) {
+      // Some Android devices can automatically process the verification code (OTP) message, and the user would NOT need to enter the code.
+      // Actually, if he/she tries to enter it, he/she will get an error message because the code was already used in the background.
+      // In this function, make sure you hide the component(s) for entering the code and/or navigate away from this screen.
+      // It is also recommended to display a message to the user informing him/her that he/she has successfully logged in.
+    }
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  // Handle the button press
+  async function signInWithPhoneNumber(phoneNumber) {
+    const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+    setConfirm(confirmation);
+  }
+
+  async function confirmCode() {
+    try {
+      await confirm.confirm(code);
+    } catch (error) {
+      console.log('Invalid code.');
+    }
+  }
+
+  if (!confirm) {
+    return (
+      <SafeAreaView>
+        <Button
+          title="Phone Number Sign In"
+          onPress={() => signInWithPhoneNumber('+1 650-555-3434')}
+        />
+      </SafeAreaView>
+    );
+  }
+
   // console.log(user);
 
   return (
@@ -50,6 +98,10 @@ const MobileSignup = () => {
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={{flex: 1}}>
         <Header />
+        <>
+          <TextInput value={code} onChangeText={text => setCode(text)} />
+          <Button title="Confirm Code" onPress={() => confirmCode()} />
+        </>
         <ScrollView>
           <View style={styles.phoneCircle}>
             <PHONE_CIRCLE />
@@ -139,7 +191,6 @@ const MobileSignup = () => {
               Please type a valid phone number
             </MText>
           )}
-          {/* <Text>hello</Text> */}
 
           <MButton
             title="Send Code"
