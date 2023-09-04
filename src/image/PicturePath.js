06d72fb3bb04021870a8731785813b81f9d27615
muin1234 <muin.ahamed@kadritech.se>
2023-09-05 02:47:44 +0600

    change image

--- src/image/PicturePath.js
+++ src/image/PicturePath.js
@@ -5,4 +5,4 @@ export const FOOD2 = require('./picture/food2.jpg');
 
 export const BURGER = require('./picture/burger.jpeg');
 export const CHILLI = require('./picture/chilli.jpeg');
-export const NAPA = require('./picture/napa.png');
+export const NAPA = require('./picture/napa.jpg');
