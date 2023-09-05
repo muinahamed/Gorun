e9fc8acb9ae85756572d75079a619d50e1700cbf
muin1234 <muin.ahamed@kadritech.se>
2023-09-06 01:01:25 +0600

    upadte

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './src/App';
